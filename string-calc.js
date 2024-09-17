a777e6e6356be3fcb0de1782866135a00b71b41f
ss-linked <181865897+ss-linked@users.noreply.github.com>
2024-09-18 03:18:25 +0530

    Define basic addition logic

--- string-calc.js
+++ string-calc.js
@@ -1,5 +1,8 @@
 function add(str) {
-    let sum;
+    let sum = 0;
+    const delimiter = ',';
+    const numbers = str.split(delimiter);
+    sum = numbers.reduce((prev, curr) => Number(prev) + Number(curr), 0);
     return sum;
 }
 
