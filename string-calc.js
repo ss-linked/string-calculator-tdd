41bbdd1f2da9a5413ad21e677355ca3eee7b1be5
ss-linked <181865897+ss-linked@users.noreply.github.com>
2024-09-18 18:18:30 +0530

    Throw error for negative numbers in input

--- string-calc.js
+++ string-calc.js
@@ -1,6 +1,10 @@
 function add(str) {
     let sum = 0;
-    let numbers = extractNumsFrom(str);
+    const numbers = extractNumsFrom(str);
+    const negativeNumbers = numbers.filter(n => Number(n) < 0);
+    if (negativeNumbers.length) {
+        throw new RangeError(`Negative numbers not allowed: ${negativeNumbers}`);
+    }
     sum = numbers.reduce((prev, curr) => prev + Number(curr), 0);
     return sum;
 
@@ -17,7 +21,19 @@ function add(str) {
             // split into 2 parts, before & after the delimiter suffix
             const customDelimiter = parsedStr.slice(0, delimiterSuffixIndex);
             parsedStr = parsedStr.slice(delimiterSuffixIndex + 1);
-            delimiters.push(customDelimiter);
+            // handle special case for delimiter clashing with negative sign
+            if (customDelimiter.startsWith('-') && customDelimiter.endsWith('-')) {
+                // prefix 0 for handling case where the first number is negative
+                parsedStr = ('0' + customDelimiter + parsedStr);
+                parsedStr = (
+                    parsedStr
+                    .replace(new RegExp(customDelimiter, 'g'), ',')
+                    .replace(new RegExp(',,', 'g'), ',-')
+                );
+            }
+            if (!delimiters.includes(customDelimiter)) {
+                delimiters.push(customDelimiter);
+            }
         }
 
         const numList = [];
@@ -25,7 +41,9 @@ function add(str) {
         for (const char of parsedStr) {
             const charCode = char.charCodeAt();
             const isNum = (charCode > 47 && charCode < 58);
-            if (isNum) currentNum += char;
+            const beginNegative = (char === '-' && !currentNum);
+            if (isNum || beginNegative)
+                currentNum += char;
             else if (delimiters.includes(char)) {
                 numList.push(currentNum);
                 currentNum = '';
