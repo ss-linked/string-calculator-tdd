a529b6eb82affc0abc9944702b894950d817633e
ss-linked <181865897+ss-linked@users.noreply.github.com>
2024-09-18 12:49:34 +0530

    Define input string number extraction logic

--- string-calc.js
+++ string-calc.js
@@ -1,9 +1,25 @@
 function add(str) {
     let sum = 0;
-    const delimiter = ',';
-    const numbers = str.split(delimiter);
-    sum = numbers.reduce((prev, curr) => Number(prev) + Number(curr), 0);
+    let numbers = splitNums(str);
+    sum = numbers.reduce((prev, curr) => prev + Number(curr), 0);
     return sum;
+
+    function splitNums(str) {
+        const delimiters = [',', '\n'];
+        const numList = [];
+        let currentNum = '';
+        for (const char of str) {
+            const charCode = char.charCodeAt();
+            const isNum = (charCode > 47 && charCode < 58);
+            if (isNum) currentNum += char;
+            else if (delimiters.includes(char)) {
+                numList.push(currentNum);
+                currentNum = '';
+            }
+        }
+        if (currentNum) numList.push(currentNum);
+        return numList;
+    }
 }
 
 module.exports = { add };
