4a0c146f89925f73e68e309d007c28c10dc9c69c
ss-linked <181865897+ss-linked@users.noreply.github.com>
2024-09-21 02:05:59 +0530

    Install vite, set up project structure

---
+++ src/calc/add.js
@@ -0,0 +1,17 @@
+import { extractNumsFrom } from './utils.js'
+
+function add(str) {
+    if (typeof str !== 'string') {
+        throw new TypeError('Only a string is allowed');
+    }
+    let sum = 0;
+    const numbers = extractNumsFrom(str);
+    const negativeNumbers = numbers.filter(n => Number(n) < 0);
+    if (negativeNumbers.length) {
+        throw new RangeError(`Negative numbers not allowed: ${negativeNumbers}`);
+    }
+    sum = numbers.reduce((prev, curr) => prev + Number(curr), 0);
+    return sum;
+}
+
+export default add;
---
+++ src/calc/functions.js
@@ -0,0 +1,10 @@
+import add from './add.js';
+
+/*
+	The calculator may have other functions
+	(subtract, multiply, divide, etc.) in the future.
+	This file is used as a central place from which
+	all these functions may be imported.
+*/
+
+export { add };
