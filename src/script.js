21d0191ff6b7d329d8061ca7c5c4756c77dd04f7
ss-linked <181865897+ss-linked@users.noreply.github.com>
2024-09-21 11:37:49 +0530

    Fix static value for selected function

--- src/script.js
+++ src/script.js
@@ -3,11 +3,12 @@ import * as calcFunctions from './calc/functions';
 const calcInput = document.getElementById('calc-input');
 const calcButton = document.getElementById('calc-button');
 const calcResult = document.getElementById('calc-result');
-const selectedFunction = document.getElementById('calc-function').value;
+const availableFunctions = document.getElementById('calc-function');
 
 calcButton.addEventListener('click', (e) => {
 	e.preventDefault();
 	try {
+		const selectedFunction = availableFunctions.value;
 		const result = calcFunctions[selectedFunction](calcInput.value);
 		calcResult.textContent = result;
 	} catch (error) {
